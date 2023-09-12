21d1684bac98d993f947693320d4e5b74678d748
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-09-12 10:21:43 -0400

    sdfsd
    fsd

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      spacing: {
+        '1': '800px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+      }
+    }
+  }
