b41e07c51c8d38d0d00dcd98d5dcfa82013f45e3
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-09-13 10:20:42 -0400

    dsf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
+  content: [
+    './home.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: 'tw-',
+
+  theme: {
+    colors: {
+      'blue': '#FF3800',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
       spacing: {
-        '1': '800px',
-        '2': '12px',
-        '3': '16px',
-        '4': '24px',
-        '5': '32px',
-        '6': '48px',
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
       }
     }
   }
+}
+
+
